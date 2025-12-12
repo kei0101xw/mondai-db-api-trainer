a35881bdbb2bca8a166369d97ab90205d8dd49dd
kei0101xw <kei0101xw@icloud.com>
2025-12-12 17:52:01 +0900

    feat: ルーティングとヘッダーコンポーネントの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
+import Solve from './pages/Solve/Solve';
 
 function App() {
   return (
     <>
-      <div>Hello World</div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/solve" element={<Solve />} />
+      </Routes>
     </>
   );
 }
