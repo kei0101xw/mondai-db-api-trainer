f9650c3f22dad51581aa4ac6745cbc7897950f55
kei0101xw <kei0101xw@icloud.com>
2025-12-14 04:10:38 +0900

    feat: ログイン・登録ページのルートを追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Solve from './pages/Solve/Solve';
+import LoginUser from './pages/LoginUser/LoginUser';
+import RegisterUser from './pages/RegisterUser/RegisterUser';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/solve" element={<Solve />} />
+        <Route path="/login" element={<LoginUser />} />
+        <Route path="/register" element={<RegisterUser />} />
       </Routes>
     </>
   );
